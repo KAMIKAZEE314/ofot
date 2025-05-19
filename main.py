4ea99bd34f87508e03dc49ba9123962f151bb317
KAMIKAZEE314 <pinguinos313@gmail.com>
2025-05-19 12:49:00 +0200

    Read File

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+import sys
+import os
+
+debug = True;
+
+with open("Dictonary.txt", "r") as file:
+    Content = file.readlines() 
+if debug:
+    print(f"Pure Zeilen:{Content}")
