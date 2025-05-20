6d9c5a7c4ac785fc7a03f193525738bf5d1da9b8
KAMIKAZEE314 <pinguinos313@gmail.com>
2025-05-20 17:51:32 +0200

    Json integration mit Test (grapes)

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 import sys
 import os
+import json
 
 debug = True;
 
-with open("Dictonary.txt", "r") as file:
-    Content = file.readlines() 
+with open("Dictonary.json", "r", encoding="utf-8") as file:
+    Dictonary = json.load(file)
 if debug:
-    print(f"Pure Zeilen:{Content}")
+    print(f"Pure Zeilen:{Dictonary}")
+    
+Dictonary["grapes"] = "Trauben"
+
+with open("Dictonary.json", "w", encoding="utf-8") as file:
+    json.dump(Dictonary, file, ensure_ascii=False, indent=4)
