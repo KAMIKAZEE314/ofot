9dbb80bc1fef29d2559680dcd5b18c9f63221899
KAMIKAZEE314 <pinguinos313@gmail.com>
2025-05-22 08:07:04 +0200

    basic mashine lerning

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import json
 from random import randint
 
-debug = True;
+debug = False;
 
 with open("Dictionary.json", "r", encoding="utf-8") as file:
     Dictionary = json.load(file)
@@ -22,21 +22,26 @@ if Dictionary.get(Request, None) == None:
     print("idk")
     if unsure.get(Request, None) == None:
         unsure[Request] = []
+        unsure_list.append(Request)
 else:
     print(f"It means {Dictionary.get(Request)} in german")
 
-rand_index = randint(0, len(unsure_list) - 1) 
-data = input(f"What's {unsure_list[rand_index]} in german: ")
-unsure[unsure_list[rand_index]].append(data)
-if len(unsure[unsure_list[rand_index]]) >= 13:
-    data = unsure[unsure_list[rand_index]]
-    same_copies = {}
-    for data_piece in data:
-        n_same_copies = -1 # -1 instead of 0 because counts itself as 1 same copy
-        for data_piece_2 in data:
-            if data_piece == data_piece_2:
-                n_same_copies += 1
-        same_copies[data_piece] = n_same_copies
+if len(unsure) != 0:
+    rand_index = randint(0, len(unsure_list) - 1)
+    if not unsure_list[rand_index] == Request:
+        data = input(f"What's {unsure_list[rand_index]} in german: ")
+        unsure[unsure_list[rand_index]].append(data)
+        if len(unsure[unsure_list[rand_index]]) >= 13:
+            data = unsure[unsure_list[rand_index]]
+            same_copies = {}
+            for data_piece in data:
+                n_same_copies = -1 # -1 instead of 0 because counts itself as 1 same copy
+                for data_piece_2 in data:
+                    if data_piece == data_piece_2:
+                        n_same_copies += 1
+                same_copies[data_piece] = n_same_copies
+            Dictionary[unsure_list[rand_index]] = max(same_copies, key=same_copies.get)
+            del unsure[unsure_list[rand_index]]
                 
 with open("Dictionary.json", "w", encoding="utf-8") as file:
     json.dump(Dictionary, file, ensure_ascii=False, indent=4)
